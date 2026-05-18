4c69481e6a9208c8e78421dc2a1d1c65edb016d0
Kazi Namira Meyheg <namirameyheg@gmail.com>
2026-05-19 03:40:22 +0600

    Refactor matrix inversion code for clarity and accuracy

--- Matrix_Inversion_Method.cpp
+++ Matrix_Inversion_Method.cpp
@@ -1,62 +1,74 @@
 #include <iostream>
 #include <iomanip>
 using namespace std;
-
 int main()
 {
-
-    float A[3][3] = {
-        {1, 1, 1},
-        {1, 2, 3},
-        {1, 3, 4}};
-
-    float B[3] = {1, 6, 6};
-
-    float det;
-
-    // Determinant
+    int n = 3;
+    double A[3][3] = {{1, 1, 1},
+                      {1, 2, 3},
+                      {1, 3, 4}};
+    double B[3] = {1, 6, 6};
+    double det;
+    cout << "Coefficient Matrix A:\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << setw(5) << A[i][j];
+        }
+        cout << endl;
+    }
     det = A[0][0] * (A[1][1] * A[2][2] - A[1][2] * A[2][1]) - A[0][1] * (A[1][0] * A[2][2] - A[1][2] * A[2][0]) + A[0][2] * (A[1][0] * A[2][1] - A[1][1] * A[2][0]);
-
+    cout << "\nDeterminant = " << det << endl;
     if (det == 0)
     {
-        cout << "Inverse does not exist";
+        cout << "Inverse does not exist (Singular Matrix)" << endl;
         return 0;
     }
+    double cof[3][3];
+    cof[0][0] = (A[1][1] * A[2][2] - A[1][2] * A[2][1]);
+    cof[0][1] = -(A[1][0] * A[2][2] - A[1][2] * A[2][0]);
+    cof[0][2] = (A[1][0] * A[2][1] - A[1][1] * A[2][0]);
 
-    float inv[3][3];
+    cof[1][0] = -(A[0][1] * A[2][2] - A[0][2] * A[2][1]);
+    cof[1][1] = (A[0][0] * A[2][2] - A[0][2] * A[2][0]);
+    cof[1][2] = -(A[0][0] * A[2][1] - A[0][1] * A[2][0]);
 
-    // Inverse matrix
-    inv[0][0] = (A[1][1] * A[2][2] - A[1][2] * A[2][1]) / det;
-    inv[0][1] = -(A[0][1] * A[2][2] - A[0][2] * A[2][1]) / det;
-    inv[0][2] = (A[0][1] * A[1][2] - A[0][2] * A[1][1]) / det;
+    cof[2][0] = (A[0][1] * A[1][2] - A[0][2] * A[1][1]);
+    cof[2][1] = -(A[0][0] * A[1][2] - A[0][2] * A[1][0]);
+    cof[2][2] = (A[0][0] * A[1][1] - A[0][1] * A[1][0]);
 
-    inv[1][0] = -(A[1][0] * A[2][2] - A[1][2] * A[2][0]) / det;
-    inv[1][1] = (A[0][0] * A[2][2] - A[0][2] * A[2][0]) / det;
-    inv[1][2] = -(A[0][0] * A[1][2] - A[0][2] * A[1][0]) / det;
-
-    inv[2][0] = (A[1][0] * A[2][1] - A[1][1] * A[2][0]) / det;
-    inv[2][1] = -(A[0][0] * A[2][1] - A[0][1] * A[2][0]) / det;
-    inv[2][2] = (A[0][0] * A[1][1] - A[0][1] * A[1][0]) / det;
-
-    float X[3];
-
-    // X = A^-1 * B
-    for (int i = 0; i < 3; i++)
+    double inv[3][3];
+    for (int i = 0; i < n; i++)
     {
+        for (int j = 0; j < n; j++)
+        {
+            inv[j][i] = cof[i][j] / det;
+        }
+    }
 
+    cout << "\nInverse Matrix :\n";
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << fixed << setprecision(2) << setw(8) << inv[i][j];
+        }
+        cout << endl;
+    }
+
+    double X[3];
+    for (int i = 0; i < n; i++)
+    {
         X[i] = 0;
-
-        for (int j = 0; j < 3; j++)
+        for (int j = 0; j < n; j++)
         {
             X[i] += inv[i][j] * B[j];
         }
     }
-
-    cout << fixed << setprecision(2);
-
+    cout << "\nSolution:\n";
     cout << "x = " << X[0] << endl;
     cout << "y = " << X[1] << endl;
     cout << "z = " << X[2] << endl;
-
     return 0;
 }
