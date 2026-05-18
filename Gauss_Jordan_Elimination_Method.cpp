af10de0e4446572c7aea43bb6bae4fb6332c1bf0
Kazi Namira Meyheg <namirameyheg@gmail.com>
2026-05-19 04:00:08 +0600

    Modify Gauss-Jordan method to accept user input
    Updated the Gauss-Jordan elimination program to take user input for the coefficients and constants of the equations.

--- Gauss_Jordan_Elimination_Method.cpp
+++ Gauss_Jordan_Elimination_Method.cpp
@@ -4,30 +4,35 @@ using namespace std;
 
 int main()
 {
+    float a[3][4]; // 3x3 coefficients + 1 column for constants
 
-    float a[3][4] = {
-        {1, 2, 1, 8},
-        {2, 3, 4, 20},
-        {4, 3, 2, 16}};
-
+    // Taking input from the user
+    cout << "Enter the coefficients and constants of the 3 equations (x y z constant):\n";
     for (int i = 0; i < 3; i++)
     {
+        cout << "Equation " << i + 1 << ": ";
+        for (int j = 0; j < 4; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
 
+    // Gauss-Jordan Elimination
+    for (int i = 0; i < 3; i++)
+    {
+        // Make the pivot 1
         float pivot = a[i][i];
-
         for (int j = 0; j < 4; j++)
         {
             a[i][j] /= pivot;
         }
 
+        // Make other elements in pivot column 0
         for (int k = 0; k < 3; k++)
         {
-
             if (k != i)
             {
-
                 float factor = a[k][i];
-
                 for (int j = 0; j < 4; j++)
                 {
                     a[k][j] -= factor * a[i][j];
@@ -36,8 +41,9 @@ int main()
         }
     }
 
+    // Display solution
     cout << fixed << setprecision(2);
-
+    cout << "\nSolution:\n";
     cout << "x = " << a[0][3] << endl;
     cout << "y = " << a[1][3] << endl;
     cout << "z = " << a[2][3] << endl;
